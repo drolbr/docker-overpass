ad4efd12f2213a19a14a118f8ee712215ab5b79d
Roland Olbricht <roland.olbricht@gmx.de>
2022-01-31 08:05:21 +0100

    Integrate automatic updates

--- overpass-api-server/run.sh
+++ overpass-api-server/run.sh
@@ -17,19 +17,22 @@ elif [[ $DB_UID -ne $PROC_UID ]]; then
 else
   cd /overpass
   bin/dispatcher --osm-base --db-dir=db/ &
+  export OVERPASS_DB_DIR=/overpass/db/
+  sleep 1
+  bin/fetch_osc_and_apply.sh https://planet.osm.org/replication/minute &
 fi  
 
 shutdown()
 {
   if [[ $PROC_UID -eq 0 ]]; then
     cd /overpass
-    ls -l bin/dispatcher
     kill $RUN_PID
     wait
   else
+    echo "Overpass user stops the dispatcher"
     cd /overpass
-    ls -l 
     bin/dispatcher --osm-base --terminate
+    rm db/*.lock
     exit 0
   fi
 };
